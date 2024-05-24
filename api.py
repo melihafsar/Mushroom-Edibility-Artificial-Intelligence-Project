ac4cd8364d2b0fca9a051389164b282df59d72d1
osmanaslancan <osmanaslancan@users.noreply.github.com>
2024-05-24 18:24:54 +0300

    add cnn to api

--- api.py
+++ api.py
@@ -5,22 +5,44 @@ from fastapi import FastAPI, File, UploadFile
 from ultralytics import YOLO
 import cv2
 import numpy as np
+from keras.models import load_model
 
 app = FastAPI()
 
 
-model = YOLO("./best.pt") 
+yolo = YOLO("./best.pt") 
+cnn = load_model("cnn.h5")
 
+
+
+def yolo_predict(image):
+    # Convert the image to a 3-dimensional numpy array
+    image_array = np.array(image)
+    print(image_array.shape)
+
+    results = yolo.predict(image)
+
+
+    return results[0].probs.data.cpu().tolist()
+
+def cnn_predict(image):
+    resized = cv2.resize(image, (128, 128), interpolation=cv2.INTER_LINEAR)
+    # Convert the image to a 3-dimensional numpy array
+    image_array = np.array(resized).reshape(1, 128, 128, 3)
+    print(image_array.shape)
+
+    results = cnn.predict(image_array)
+
+    return results.tolist()
 
 @app.post("/predict")
 def read_item(file: Annotated[UploadFile, File()]):
     # Read the image using OpenCV
     image = cv2.imdecode(np.frombuffer(file.file.read(), np.uint8), cv2.IMREAD_COLOR)
-    # Convert the image to a 3-dimensional numpy array
-    image_array = np.array(image)
-    print(image_array.shape)
 
-    results = model.predict(image_array)
-    
+    yolo = yolo_predict(image)
+
+    cnn = cnn_predict(image)
+
     # Return the result
-    return {"yolo": {"probs": results[0].probs.data.cpu().tolist(), "names": results[0].names}}
+    return {"yolo": yolo, "cnn": cnn, "names": ["edible", "poisonous"]}
